1249aaa9de73c519c4dd70de9f22db352a7df42c
javaughn-bradford <javaughn.bradford001@albright.edu>
2025-09-26 14:32:10 -0400

    Initial commit

---
+++ simple_sorts.py
@@ -0,0 +1,19 @@
+# simple_sorts.py
+# bubble sort, selection sort, insertion sort
+# Strongly suggest you turn off LLMs like
+# GitHub Copilot, TabNine, etc. when working on this file.
+# Writing a sorting algorithm yourself is the best
+# way to learn how it works.
+# Modified by: 
+
+def bubble_sort(lst):
+    # YOUR CODE HERE
+    pass
+
+def selection_sort(lst):
+    # YOUR CODE HERE
+    pass
+
+def insertion_sort(lst):
+    # YOUR CODE HERE
+    pass
